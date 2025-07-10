a4b0f4fed456c8ac06d1f94f306557a98b6ba09d
DEM1G0DSK <136793567+DEM1G0DSK@users.noreply.github.com>
2025-07-10 12:29:30 +0545

    table of 5 code

---
+++ tableof5.py
@@ -0,0 +1,10 @@
+base = int(input("enter any number: "))
+
+for i in range (1, 11):
+    result = base * i
+    print(f"{base} x {i} = {result}")
+
+num1 = int(input("enter any number: "))
+num2 = int(input("enter any number: "))
+sum = num1 + num2
+print(sum) 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+base = int(input("enter any number: "))
+
+for i in range (1, 11):
+    result = base * i
+    print(f"{base} x {i} = {result}")
